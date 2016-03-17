d65b6effd96dfcc806877a58176369db9d96592a
Mariusz Saramak <mariusz.saramak@tomtom.com>
2016-03-17 12:23:45 +0100

    Test should display hello world

--- app/src/androidTest/java/com/tomtom/ndkex/conference/MainActivityTest.java
+++ app/src/androidTest/java/com/tomtom/ndkex/conference/MainActivityTest.java
@@ -2,11 +2,15 @@ package com.tomtom.ndkex.conference;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import org.junit.Test;
+
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public MainActivityTest(Class<MainActivity> activityClass) {
         super(activityClass);
     }
+
+    @Test
     public void testShouldDisplayHelloWorldMessage(){
         assertEquals(getActivity().message.getText().toString(), "Hello World");
 
